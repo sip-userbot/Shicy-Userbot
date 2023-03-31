2fd27421b42ca3fc4f9417edc6720d278f4b6922
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-31 11:53:00 +0700

    Update __init__.py

--- ShicyXd/__init__.py
+++ ShicyXd/__init__.py
@@ -323,61 +323,65 @@ except Exception as e:
 
 if STRING_2:
     session2 = StringSession(str(STRING_2))
-    CHIY2 = TelegramClient(
-        session=session2,
-        api_id=API_KEY,
-        api_hash=API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
+    try:
+        CHIY2 = TelegramClient(
+              session=session2,
+              api_id=API_KEY,
+              api_hash=API_HASH,
+              connection=ConnectionTcpAbridged,
+              auto_reconnect=True,
+              connection_retries=None,
+        )
     except Exception as e:
-    print(f"STRING_SESSION - {e}")
-    sys.exit()
+        print(f"STRING_SESSION - {e}")
+        sys.exit()
 
 
 if STRING_3:
     session3 = StringSession(str(STRING_3))
-    CHIY3 = TelegramClient(
-        session=session3,
-        api_id=API_KEY,
-        api_hash=API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
+    try:
+        CHIY3 = TelegramClient(
+              session=session3,
+              api_id=API_KEY,
+              api_hash=API_HASH,
+              connection=ConnectionTcpAbridged,
+              auto_reconnect=True,
+              connection_retries=None,
+        )
     except Exception as e:
-    print(f"STRING_SESSION - {e}")
-    sys.exit()
+        print(f"STRING_SESSION - {e}")
+        sys.exit()
 
 
 if STRING_4:
     session4 = StringSession(str(STRING_4))
-    CHIY4 = TelegramClient(
-        session=session4,
-        api_id=API_KEY,
-        api_hash=API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
+    try:
+        CHIY4 = TelegramClient(
+              session=session4,
+              api_id=API_KEY,
+              api_hash=API_HASH,
+              connection=ConnectionTcpAbridged,
+              auto_reconnect=True,
+              connection_retries=None,
+        )
     except Exception as e:
-    print(f"STRING_SESSION - {e}")
-    sys.exit()
+        print(f"STRING_SESSION - {e}")
+        sys.exit()
 
 if STRING_5:
     session5 = StringSession(str(STRING_5))
-    CHIY5 = TelegramClient(
-        session=session5,
-        api_id=API_KEY,
-        api_hash=API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
+    try:
+        CHIY2 = TelegramClient(
+              session=session2,
+              api_id=API_KEY,
+              api_hash=API_HASH,
+              connection=ConnectionTcpAbridged,
+              auto_reconnect=True,
+              connection_retries=None,
+        )
     except Exception as e:
-    print(f"STRING_SESSION - {e}")
-    sys.exit()
+        print(f"STRING_SESSION - {e}")
+        sys.exit()
 
 
 if BOT_TOKEN is not None:
